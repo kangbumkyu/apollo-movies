445da0ed6af46c1e332e4b0b6b294912e5786744
Kangbumkyu <g.kangbumkyu@gmail.com>
2021-02-17 09:28:08 +0700

    GET MOVIES Query && GET MOVIE QUERY

---
+++ src/components/Movie.js
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom";
+
+export default ({id}) => (
+    <div>
+        <Link to={`/${id}`}>
+            {id}
+        </Link> 
+    </div>
+);
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,1 +1,22 @@
-export default () => <h1>Home</h1>;
+import { gql, useQuery } from '@apollo/client'
+import Movie from '../components/Movie';
+
+const GET_MOVIES = gql`
+{
+    movies {
+        id
+        medium_cover_image
+    }
+}
+`
+
+export default () => {
+    const { data, loading, error } = useQuery(GET_MOVIES);
+    console.log(loading, data);
+    if (loading) {
+        return "loading...";
+    }
+    if (data && data.movies) {
+        return data.movies.map(movie => <Movie key={movie.id} id={movie.id} />);
+    }
+};
